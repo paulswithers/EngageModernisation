d3c4239b7362b22a5dc92a94343015ee56c20e9c
Paul Withers <paulswithers@hotmail.co.uk>
2019-05-06 09:30:23 +0100

    Completing dev for Engage

--- notes/Code/Java/extlib/ExcelUtils.java
+++ notes/Code/Java/extlib/ExcelUtils.java
@@ -1,5 +1,16 @@
 package extlib;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Map;
+
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.apache.poi.hssf.usermodel.HSSFCell;
 import org.apache.poi.hssf.usermodel.HSSFCellStyle;
 import org.apache.poi.hssf.usermodel.HSSFFont;
@@ -7,7 +18,21 @@ import org.apache.poi.hssf.usermodel.HSSFRow;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 
+import com.ibm.commons.util.io.json.JsonJavaArray;
+import com.ibm.commons.util.io.json.JsonJavaFactory;
+import com.ibm.commons.util.io.json.JsonJavaObject;
+import com.ibm.commons.util.io.json.JsonParser;
+import com.ibm.domino.services.HttpServiceConstants;
+import com.ibm.xsp.extlib.util.ExtLibUtil;
+import com.ibm.xsp.http.io.output.ByteArrayOutputStream;
+import com.ibm.xsp.webapp.XspHttpServletResponse;
+
+import lotus.domino.Database;
 import lotus.domino.Document;
+import lotus.domino.MIMEEntity;
+import lotus.domino.MIMEHeader;
+import lotus.domino.Session;
+import lotus.domino.Stream;
 
 public class ExcelUtils {
 
@@ -77,6 +102,123 @@ public class ExcelUtils {
 		HSSFCell hCell = row.getCell(0);
 		hCell.setAsActiveCell();
 		return sheet;
+	}
+
+	public static void exportSelectedContacts() {
+		try {
+			Map<String, Object> sessionScope = ExtLibUtil.getSessionScope();
+			HSSFWorkbook wb = createExcel();
+
+			HSSFSheet sheet = wb.getSheetAt(0);
+			String[] ids = (String[]) sessionScope.get("ids");
+			Database db = ExtLibUtil.getCurrentDatabase();
+			int rownum = 1;
+			for (String id : ids) {
+				Document doc = db.getDocumentByID(id);
+				writeContact(sheet, doc, rownum);
+				rownum += rownum;
+			}
+
+			finaliseWorkBook(sheet);
+
+			// Write out to servletResponse
+			ExternalContext ec = FacesContext.getCurrentInstance().getExternalContext();
+			HttpServletResponse pageResponse = (HttpServletResponse) ec.getResponse();
+			ServletOutputStream pageOutput = pageResponse.getOutputStream();
+			pageResponse.setContentType("application/x-ms-excel");
+			pageResponse.setHeader("Cache-Control", "no-cache");
+			pageResponse.setHeader("Content-Disposition", "inline; filename=monthlyReport.xls");
+			wb.write(pageOutput);
+			pageOutput.flush();
+			pageOutput.close();
+
+			//  Terminate the request processing lifecycle.
+			FacesContext.getCurrentInstance().responseComplete();
+		} catch (Throwable t) {
+			t.printStackTrace();
+		}
+	}
+
+	public static void emailSelectedContacts() {
+		FacesContext ctx = FacesContext.getCurrentInstance();
+		ExternalContext ext = ctx.getExternalContext();
+		HttpServletRequest request = (HttpServletRequest) ext.getRequest();
+		XspHttpServletResponse response = (XspHttpServletResponse) ext.getResponse();
+		try {
+
+			if (request.getMethod() != HttpServiceConstants.HTTP_POST) {
+				response.setStatus(HttpServletResponse.SC_METHOD_NOT_ALLOWED);
+			} else if (null == request.getHeader("secretKey")) {
+				response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+			} else if (!"d1p243eafde5272".equals(request.getHeader("secretKey"))) {
+				response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+			} else if (null == request.getHeader("sendTo")) {
+				response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+			} else {
+				InputStreamReader isR = new InputStreamReader(request.getInputStream());
+				JsonJavaObject jsonObj = (JsonJavaObject) JsonParser.fromJson(JsonJavaFactory.instanceEx, isR);
+				JsonJavaArray ids = jsonObj.getAsArray("data");
+
+				response.setContentType("application/json");
+				response.setHeader("Cache-Control", "no-cache");
+
+				HSSFWorkbook wb = createExcel();
+
+				HSSFSheet sheet = wb.getSheetAt(0);
+				Database db = ExtLibUtil.getCurrentDatabase();
+				int rownum = 1;
+				for (Object id : ids) {
+					Document doc = db.getDocumentByUNID((String) id);
+					writeContact(sheet, doc, rownum);
+					rownum += rownum;
+				}
+
+				finaliseWorkBook(sheet);
+
+				ByteArrayOutputStream os = new ByteArrayOutputStream(1024);
+				wb.write(os);
+
+				Session session = ExtLibUtil.getCurrentSessionAsSigner();
+				session.setConvertMime(false);
+				Database mailBox = session.getDatabase(session.getServerName(), "mail.box");
+				Document memo = mailBox.createDocument();
+				memo.replaceItemValue("RecNoOutOfOffice", "1"); //no replies from out of office agents
+				String mimeBoundary = memo.getUniversalID().toLowerCase();
+
+				MIMEEntity mimeRoot = memo.createMIMEEntity("Body");
+
+				MIMEHeader mimeHeader;
+				mimeHeader = mimeRoot.createHeader("To");
+				mimeHeader.setHeaderVal(request.getHeader("sendTo"));
+
+				//set subject
+				mimeHeader = mimeRoot.createHeader("Subject");
+				mimeHeader.addValText("Contacts Export", "UTF-8");
+
+				MIMEEntity emailRootChild = mimeRoot.createChildEntity();
+				mimeHeader = emailRootChild.createHeader("Content-Type");
+
+				mimeHeader = emailRootChild.createHeader("Content-Disposition");
+				mimeHeader.setHeaderVal("attachment; filename=\"Contacts.xls\"");
+				InputStream is = new ByteArrayInputStream(os.toByteArray());
+				Stream stream = session.createStream();
+				stream.setContents(is);
+				emailRootChild.setContentFromBytes(stream, "application/vnd.ms-excel", MIMEEntity.ENC_IDENTITY_BINARY);
+				memo.closeMIMEEntities(true);
+				memo.send();
+
+				session.setConvertMime(true);
+
+				response.setStatus(HttpServletResponse.SC_OK);
+			}
+
+			//  Terminate the request processing lifecycle.
+			FacesContext.getCurrentInstance().responseComplete();
+		} catch (Throwable t) {
+			t.printStackTrace();
+			response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+			FacesContext.getCurrentInstance().responseComplete();
+		}
 	}
 
 }
